d72e97bb0ced4d3f613cf9265ac66315ebed06b6
5cuong <hoangcuong170825@gmail.com>
2025-07-20 22:47:40 +0700

    Theme.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import  ThreeDRotation  from '@mui/icons-material/ThreeDRotation'
+import  Typography  from '@mui/material/Typography'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <div>5cuong</div>
+    <Typography variant="body2" color="text.secondary">Test Typography</Typography>
       <Button variant="contained">Hello workd</Button>
       <Button variant="outlined">Outlined</Button>
       <Button variant="text">Text</Button>
